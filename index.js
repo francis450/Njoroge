838145dcc1d3e0bd6d1c734677fbf089234a8bda
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-13 19:45:25 +0100

    Fix eslint errors

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const menu = document.querySelector('.menu');
 const menuList = menu.querySelectorAll('li');
 const checkbox = document.getElementById('menu-btn');
 
-menuList.forEach(item => {
-    item.addEventListener('click', () => {
-        checkbox.checked = false;
-    });
+menuList.forEach((item) => {
+  item.addEventListener('click', () => {
+    checkbox.checked = false;
+  });
 });
