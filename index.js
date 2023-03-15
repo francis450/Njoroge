d0b905f8bf8dee3a7b900f72555643fa8ddf1417
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-15 09:07:49 +0100

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.menu-icon');
-const menuItem = document.querySelector('.menu-item')
 
-menuBtn.addEventListener('click', function() {
+function controlChecked() {
+  menu.style.maxHeight = '240px';
+  menuIcon.querySelector('.navicon').style.background = 'transparent';
+}
+
+function controlUnchecked() {
+  menuBtn.checked = false;
+  menu.style.maxHeight = null;
+  menuIcon.querySelector('.navicon').style.background = null;
+}
+
+menuBtn.addEventListener('click', () => {
   if (this.checked) {
     controlChecked();
   } else {
     controlUnchecked();
   }
 });
-function controlChecked(){
-  menu.style.maxHeight = '240px';
-  menuIcon.querySelector('.navicon').style.background = 'transparent';
-}
-
-function controlUnchecked(){
-  menuBtn.checked = false;
-  menu.style.maxHeight = null;
-  menuIcon.querySelector('.navicon').style.background = null;
-}
-
-
-function removeChecked(){
-  controlUnchecked();
-}
