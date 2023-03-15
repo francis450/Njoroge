0cd11a6d0cf8b26c170f5a547fcaff56c15d306a
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-15 12:36:11 +0100

    Correct working

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.menu-icon');
+const menuItem = document.querySelector('.menu-item')
 
-function controlChecked() {
-  menu.style.maxHeight = '240px';
-  menuIcon.querySelector('.navicon').style.background = 'transparent';
-}
-
-function controlUnchecked() {
-  menuBtn.checked = false;
-  menu.style.maxHeight = null;
-  menuIcon.querySelector('.navicon').style.background = null;
-}
-
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', function() {
   if (this.checked) {
     controlChecked();
   } else {
     controlUnchecked();
   }
 });
+function controlChecked(){
+  menu.style.maxHeight = '240px';
+  menuIcon.querySelector('.navicon').style.background = 'transparent';
+}
+
+function controlUnchecked(){
+  menuBtn.checked = false;
+  menu.style.maxHeight = null;
+  menuIcon.querySelector('.navicon').style.background = null;
+}
+
+
+function removeChecked(){
+  controlUnchecked();
+}
