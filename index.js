96d9fa1efe1e1c736fb3a118da9819ee324dfdb8
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-15 12:56:41 +0100

    Fix minor eslint errors

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.menu-icon');
-const menuItem = document.querySelector('.menu-item')
+const menuItem = document.querySelectorAll('.menu-item')
+menuBtn.addEventListener('click', check);
 
-menuBtn.addEventListener('click', function() {
-  if (this.checked) {
-    controlChecked();
-  } else {
-    controlUnchecked();
-  }
-});
-function controlChecked(){
+function controlChecked() {
   menu.style.maxHeight = '240px';
   menuIcon.querySelector('.navicon').style.background = 'transparent';
 }
 
-function controlUnchecked(){
+function controlUnchecked() {
   menuBtn.checked = false;
   menu.style.maxHeight = null;
   menuIcon.querySelector('.navicon').style.background = null;
 }
 
+function check() {
+  if (this.checked) {
+    controlChecked();
+  } else {
+    controlUnchecked();
+  }
+}
 
-function removeChecked(){
+menuItem.forEach(element => {
+  element.addEventListener('click',removeChecked);
+});
+
+function removeChecked() {
   controlUnchecked();
 }
