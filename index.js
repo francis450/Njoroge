9c1bb4b39087a07be12d5b3338d6ee4bb9a78283
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-16 22:44:39 +0100

    Create an object and save data to local storage

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const name = document.getElementById('full-name');
+// Define an object to store form data
+const formData = {};
+
+// Get all the input fields in the form
+const formInputs = document.querySelectorAll('.input');
+
+// Loop through each input field and add an event listener to save data to local storage
+formInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    formData[input.name] = input.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+});
+
+// Check if there is any data in local storage and pre-fill the input fields with that data
+if (localStorage.getItem('formData')) {
+  const storedFormData = JSON.parse(localStorage.getItem('formData'));
+  formInputs.forEach(input => {
+    if (storedFormData[input.name]) {
+      input.value = storedFormData[input.name];
+    }
+  });
+}
