e9f9f7265ed73e18d81f96267cc01dae6a53b044
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-17 09:26:56 +0100

    validate

--- index.js
+++ index.js
@@ -2,11 +2,3 @@ const form = document.querySelector('#form');
 const email = document.querySelector('#email-input');
 const errorMsg = document.querySelector('.error-message');
 
-form.onsubmit= () => {
-    if(email.value === email.value.toLowerCase()) {
-        return true;
-    } else {
-        errorMsg.style.display = 'block';
-        return false;
-    }
-};
