e42940afb3c42bfb892da599b83469c3b82b79fe
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-17 10:45:41 +0100

    Fix eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('#form');
 const email = document.querySelector('#email-input');
 const errorMsg = document.querySelector('.error-message');
-
+var state = false
 form.onsubmit = () => {
   if (email.value === email.value.toLowerCase()) {
-    return true;
+    state = true;
   } else {
     errorMsg.style.display = 'block';
-    return false;
-}
+  }
+  return state;
 };
