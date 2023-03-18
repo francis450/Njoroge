5caad24bba7ee349eb0b8125474e8bd989cbe0bc
Francis Njoroge <franciskamande2001@gmail.com>
2023-03-18 11:03:05 +0100

    Fix more eslint

--- index.js
+++ index.js
@@ -3,49 +3,100 @@ const modal = document.getElementById('popup-container');
 
 // create cards object
 const details = {
-  card1 : {
+  card1: {
     id: 0,
-    name:'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory',
     decription: 'Aenean at lorem purus. Suspendisse vitae libero dui. Phasellus placerat posuere lacus, in auctor diam mollis ac. Duis congue tincidunt ante vel pretium. Suspendisse porttitor ipsum id dolor convallis varius. Donec faucibus neque nec ex rutrum mollis. Nulla sit amet efficitur augue, sit amet tristique ante. Nunc non condimentum dui. Aliquam mattis pharetra magna ut porttitor. Proin suscipit nunc eget massa pharetra, vitae rhoncus purus dictum. Suspendisse vel tellus hendrerit, lacinia ligula vitae, egestas metus. Nunc quis nisi ut metus accumsan rhoncus.',
     image: 'img/cards-images/cardone.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
-  card2 : {
+  card2: {
     id: 1,
-    name:'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory',
     decription: 'Nunc lobortis augue in efficitur porttitor. Cras tincidunt sagittis dapibus. In at erat quis massa pellentesque ullamcorper vitae et lorem. Fusce nisi velit, elementum ut luctus nec, vulputate in tellus. Quisque pulvinar ac ipsum fringilla rhoncus. Nullam bibendum diam ligula, eu venenatis nisi euismod a. Sed tincidunt sit amet lectus eget efficitur.',
     image: 'img/cards-images/cardtwo.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
-  card3 : {
+  card3: {
     id: 2,
-    name:'Uber App',
+    name: 'Uber App',
     decription: 'Vivamus mattis, arcu at vulputate pellentesque, ante ex gravida risus, et luctus dolor tortor vitae libero. Sed sollicitudin leo id eros vestibulum lacinia. Duis tincidunt, ligula id elementum commodo, nulla nibh fringilla justo, ac hendrerit turpis justo a arcu. Aenean euismod nibh at tellus tincidunt tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ac facilisis dui. ',
     image: 'img/cards-images/cardthree.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
-  card4 : {
+  card4: {
     id: 3,
-    name:'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory',
     decription: 'lorem Ipsum',
     image: 'img/cards-images/cardfour.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
-  card5 : {
+  card5: {
     id: 4,
-    name:'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory',
     decription: 'Etiam volutpat lacus non rhoncus porta. Etiam vulputate cursus libero, quis semper odio. Etiam interdum tortor ac orci sagittis, sit amet dictum arcu porta. Proin convallis dui lacus, vitae tincidunt magna finibus id. Sed velit quam, malesuada ut tristique sit amet, suscipit ut ex. In laoreet sed mauris id rhoncus. Pellentesque tempor elementum lorem, eu aliquam ex fringilla non. Fusce mollis commodo sem tempus fermentum.',
     image: 'img/cards-images/cardfive.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
-  card6 : {
+  card6: {
     id: 5,
-    name:'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory',
     decription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in ligula dictum, volutpat ante id, consectetur leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel felis faucibus, gravida mi tincidunt, rhoncus risus. Aenean odio velit, mattis eu diam vel, tincidunt ornare mi. In fermentum metus quam, id dignissim libero accumsan in. Ut condimentum ultricies mollis. Aenean at ante ut magna posuere fringilla.',
     image: 'img/cards-images/cardsix.jpg',
-    technology:['ruby on rails', 'css', 'javascript', 'html']
+    technology:['ruby on rails', 'css', 'javascript', 'html'],
   },
 };
+
+function popit(cardN) {
+  const modalBody = `
+    <div class='popup-card'>
+        <div class='close-button-container'>
+            <button src='img/Disabled.svg' id='close-button' alt=''>&times</button>
+        </div>
+        <div class='image-container'>
+            <img src='${cardN.image}' class='popup-image' alt='Card Image'>
+        </div>  
+        <div class='popup-title-container'>
+            <p class='popup-title-text'>${cardN.name}</p>
+            <div class='popup-buttons'>
+                <button class='see-button'>
+                    <p class='see-button-text'>See Live</p>
+                    <img src='img/popup-live.svg'  alt='' class='see-live-icon'>
+                </button>
+                <button class='see-button'>
+                    <p class='see-button-text'>See Source</p>
+                    <img src='img/popup-github.svg' alt='' class='see-source-icon'>
+                </button>
+            </div>
+        </div>
+        <ul class='card-tags2'>
+            <li class='card-tag ruby'><p class='tech-item-text'>Ruby on rails</p></li>
+            <li class='card-tag html'><p class='tech-item-text'>html</p></li>
+            <li class='card-tag css'><p class='tech-item-text'>css</p></li>
+            <li class='card-tag javascript'><p class='tech-item-text'>javascript</p></li>
+        </ul>
+        <div class='description-container'>
+            <p class='description'>${cardN.decription}</p>
+        </div>
+        <div class='popup-buttons2'>
+            <button class='see-button'>
+                <p class='see-button-text'>See Live</p>
+                <img src='img/popup-live.svg'  alt='' class='see-live-icon'>
+            </button>
+            <button class='see-button'>
+                <p class='see-button-text'>See Source</p>
+                <img src='img/popup-github.svg' alt='' class='see-source-icon'>
+            </button>
+        </div>
+    </div>
+    `;
+  modal.innerHTML = modalBody;
+  modal.style.display = 'flex';
+  const close = document.getElementById('close-button');
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+}
 
 // Dynamically create the work section
 function createCard(cardN) {
@@ -85,10 +136,11 @@ function createCard(cardN) {
   cardButton.setAttribute('class', 'card-button');
   cardButtonText.setAttribute('class', 'card-button-text');
 
-  tag1.textContent = cardN.technology[0];
-  tag2.textContent = cardN.technology[1];
-  tag3.textContent = cardN.technology[2];
-  tag4.textContent = cardN.technology[3];
+  const [tech1, tech2, tech3, tech4] = cardN.technology;
+  tag1.textContent = tech1;
+  tag2.textContent = tech2;
+  tag3.textContent = tech3;
+  tag4.textContent = tech4;
   card.style.backgroundImage = 'url(' + cardN.image + ')';
   cardButtonText.textContent = 'See Project';
 
@@ -152,54 +204,3 @@ window.addEventListener('load', () => {
   works.appendChild(createWorksTitleContainer());
   works.append(createWorkCardsContainer());
 });
-
-function popit(cardN) {
-  const modalBody = `
-    <div class='popup-card'>
-        <div class='close-button-container'>
-            <button src='img/Disabled.svg' id='close-button' alt=''>&times</button>
-        </div>
-        <div class='image-container'>
-            <img src='${cardN.image}' class='popup-image' alt='Card Image'>
-        </div>  
-        <div class='popup-title-container'>
-            <p class='popup-title-text'>${cardN.name}</p>
-            <div class='popup-buttons'>
-                <button class='see-button'>
-                    <p class='see-button-text'>See Live</p>
-                    <img src='img/popup-live.svg'  alt='' class='see-live-icon'>
-                </button>
-                <button class='see-button'>
-                    <p class='see-button-text'>See Source</p>
-                    <img src='img/popup-github.svg' alt='' class='see-source-icon'>
-                </button>
-            </div>
-        </div>
-        <ul class='card-tags2'>
-            <li class='card-tag ruby'><p class='tech-item-text'>Ruby on rails</p></li>
-            <li class='card-tag html'><p class='tech-item-text'>html</p></li>
-            <li class='card-tag css'><p class='tech-item-text'>css</p></li>
-            <li class='card-tag javascript'><p class='tech-item-text'>javascript</p></li>
-        </ul>
-        <div class='description-container'>
-            <p class='description'>${cardN.decription}</p>
-        </div>
-        <div class='popup-buttons2'>
-            <button class='see-button'>
-                <p class='see-button-text'>See Live</p>
-                <img src='img/popup-live.svg'  alt='' class='see-live-icon'>
-            </button>
-            <button class='see-button'>
-                <p class='see-button-text'>See Source</p>
-                <img src='img/popup-github.svg' alt='' class='see-source-icon'>
-            </button>
-        </div>
-    </div>
-    `;
-  modal.innerHTML = modalBody;
-  modal.style.display = 'flex';
-  const close = document.getElementById('close-button');
-  close.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-}
